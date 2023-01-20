882c11587fc18d83a2b9a4da07c715f4f0fd2713
daniel2005erm <iliaz1986kg1986@gmail.com>
2023-01-20 18:55:59 +0600

    first commit

---
+++ hero.py
@@ -0,0 +1,24 @@
+# class SuperHero:
+#     people='people'
+#     def init(self,name,nickname,supername, health_points,catchphrase) -> None:
+#         self.name = name
+#         self.nickname = nickname
+#         self.supername = supername
+#         self.health_points = health_points
+#         self.catchphrase = catchphrase
+#     def info(self):
+#         return f"Name: {self.name}, Catchphrase: {self.catchphrase}"
+#     def healthpoints(self):
+#         return f"Имя: {self.name}, его здоровье {self.health_points * 2}"
+
+#     def str(self):
+#         return f"nick: {self.nickname}, super-power: {self.supername}, health: {self.health_points}, catchphrase: {self.catchphrase}"
+#     def len(self):
+#         return len(self.catchphrase)
+
+    
+# hero = SuperHero("askhat","python", "prog",100,"programmist")
+# print(hero.info())
+# print(hero)
+# print(hero.healthpoints())
+# print(hero.len())
