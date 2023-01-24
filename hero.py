b1ed170487ee6bc013a12a9ba7a22c47bf7c799b
daniel2005erm <iliaz1986kg1986@gmail.com>
2023-01-24 18:53:23 +0600

    first commit

--- hero.py
+++ hero.py
@@ -1,24 +1,45 @@
-# class SuperHero:
-#     people='people'
-#     def init(self,name,nickname,supername, health_points,catchphrase) -> None:
-#         self.name = name
-#         self.nickname = nickname
-#         self.supername = supername
-#         self.health_points = health_points
-#         self.catchphrase = catchphrase
-#     def info(self):
-#         return f"Name: {self.name}, Catchphrase: {self.catchphrase}"
-#     def healthpoints(self):
-#         return f"Имя: {self.name}, его здоровье {self.health_points * 2}"
+class SuperHero:
+    people='people'
+    def init(self,name,nickname,supername, health_points,catchphrase) -> None:
+        self.name = name
+        self.nickname = nickname
+        self.supername = supername
+        self.health_points = health_points
+        self.catchphrase = catchphrase
+    def info(self):
+        return f"Name: {self.name}, Catchphrase: {self.catchphrase}"
+    def healthpoints(self):
+        return f"Имя: {self.name}, его здоровье {self.health_points * 2}"
 
-#     def str(self):
-#         return f"nick: {self.nickname}, super-power: {self.supername}, health: {self.health_points}, catchphrase: {self.catchphrase}"
-#     def len(self):
-#         return len(self.catchphrase)
+    def str(self):
+        return f"nick: {self.nickname}, super-power: {self.supername}, health: {self.health_points}, catchphrase: {self.catchphrase}"
+    def len(self):
+        return len(self.catchphrase)
 
     
-# hero = SuperHero("askhat","python", "prog",100,"programmist")
-# print(hero.info())
-# print(hero)
-# print(hero.healthpoints())
-# print(hero.len())
+hero = SuperHero("askhat","python", "prog",100,"programmist")
+
+class NewSuper(SuperHero):
+    def init(self, name, nickname, supername, health_points, catchphrase) -> None:
+         super().init(name, nickname, supername, health_points, catchphrase)
+         self.damage = False
+         self.fly = False
+    def kvodrat(self):
+        self.fly = True
+        return f"Квадрат: {self.health_points ** 2}, Fly: {self.fly}"
+    def newmetod(self):
+        return f"newdamage: {self.damage}, newfly: {self.fly}"
+    def flyg(self):
+        return f"catchphrase: {self.catchphrase}"
+newsuper = NewSuper("askhat", "spider", "web",6,"fly in the True phrase")
+print(newsuper.kvodrat())
+print(newsuper.flyg())
+print(newsuper.newmetod())
+class Villain(SuperHero):
+    people = "monstr"
+    def gen_x(self):
+        pass
+    def crit(self,damage1,damage2):
+        return f"uron: {damage1 , damage2}"
+villain = Villain("nurbolot", "kot", "sperma",100,"sperma man")
+print(villain.crit(6,10))
